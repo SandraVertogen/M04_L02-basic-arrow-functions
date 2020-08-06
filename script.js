7381d270171aa51ba001f20cdc3d382fc9dc89b6
Sandra Vertogen <sandra28091984@hotmail.com>
2020-08-06 16:04:54 +0200

    opdracht toevoegen

---
+++ script.js
@@ -0,0 +1,23 @@
+//Deel A
+const ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!");
+ikRockArrowFunctions();
+
+//Deel B
+const fivePlusSeven = () => 5 + 7;
+fivePlusSeven();
+
+//Deel C
+() => 1 + 2;
+
+//Deel D
+const myFunction = (a, b) => a + b;
+
+//Deel E
+const addFive = a => a + 5;
+
+//Deel F
+const createObject = () => ({ greeting: "Hoi" });
+
+
+
+
